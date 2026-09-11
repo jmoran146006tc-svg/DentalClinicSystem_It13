e14c2fb0b9cc219ac11f0cbff475c2f5009f9b1c
damonmaitsu76-cmyk <damonmaitsu76@gmail.com>
2026-09-11 18:02:27 +0800

    Add DbConnectionHelper for async MySQL connections
    Added Helpers/DbConnectionHelper for async MySQL access using MySqlConnector.

---
+++ DentalClinicSystem/Helpers/DbConnectionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySqlConnector;
+
+namespace DentalClinicSystem.Helpers
+{
+
+    public static class DbConnectionHelper
+    {
+        private const string ConnectionString =
+            "Server=localhost;Database=dentalclinicdb;User=root;Password=;";
+
+
+        public static async Task<MySqlConnection> GetOpenConnectionAsync()
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
